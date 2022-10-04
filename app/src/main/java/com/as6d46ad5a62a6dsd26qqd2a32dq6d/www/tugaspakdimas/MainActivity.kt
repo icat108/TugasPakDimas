d87f2fedf6a1d9f5dfe069b49a857eea9f7ddc62
icat108 <ichacnakval@gmail.com>
2022-10-04 18:44:24 +0700

    this

--- app/src/main/java/com/as6d46ad5a62a6dsd26qqd2a32dq6d/www/tugaspakdimas/MainActivity.kt
+++ app/src/main/java/com/as6d46ad5a62a6dsd26qqd2a32dq6d/www/tugaspakdimas/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.as6d46ad5a62a6dsd26qqd2a32dq6d.www.tugaspakdimas
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         //membuat aksi ketika button di klik
         val tombolAksi: Button = findViewById(R.id.textButton)
@@ -23,24 +23,31 @@ class MainActivity : AppCompatActivity() {
 
     //fungsi ketika tombol di klik akan di panggil fungsi ini
     private fun ubahTeks () {
-        Toast.makeText(this , "BUTTON CLICKED",
-            Toast.LENGTH_SHORT).show()
+//        Toast nya saya comand supaya 'BUTTON CLICKED' tidak tampak ketika di klik disini
+//        Toast.makeText(this , "BUTTON CLICKED",
+//            Toast.LENGTH_SHORT).show()
 
 
         //mengubah isis tet box
         val txthello: TextView = findViewById(R.id.txthello)
         val txtdadu2: TextView = findViewById(R.id.txtdadu2)
         val txtdadu3: TextView = findViewById(R.id.txtdadu3)
+        val txtdadu4: TextView = findViewById(R.id.txtdadu4)
 
-        val randomInt = (1..500).random()
-        val tes = (1 until 500).random()
-        val tes2 = (1 until 500).random()
-        val tes3 = (1 until 500).random()
+        val randomInt = (1..954125).random()
+        val tes = (1 until 954125).random()
+        val tes2 = (1 until 954125).random()
+        val tes3 = (1 until 954125).random()
+        val tes4 = (1 until 954125).random()
 
         txthello.text = tes.toString()
         txtdadu2.text = tes2.toString()
         txtdadu3.text = tes3.toString()
+        txtdadu4.text = tes4.toString()
 
     }
+
+
+
 
 }
